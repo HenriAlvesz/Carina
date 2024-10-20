7c8d23e037c9c83efa850304d01ad10a89b777ea
HenriAlvesz <gh8331502@gmail.com>
2024-10-20 10:51:14 -0300

    add a função de falar horas
    overflow resolvido

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import pyaudio
 import pyttsx3
 import json
-
+import core
 # Síntese de fala
 engine = pyttsx3.init()
 
@@ -13,25 +13,41 @@ engine.setProperty('voice', voices[-2].id)
 def speak(text):
     engine.say(text)
     engine.runAndWait()
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
+import pyttsx3
+import json
 
+# Síntese de fala
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+# Reconhecimento de fala
 model = Model('C:/Users/Alexssandra Bezerra/OneDrive/Desktop/Carina/Carina/model')
-
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=1024)
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(1024)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
         result = rec.Result()
         result = json.loads(result)
 
-        if result is not None:
+        if 'text' in result and result['text']:  # Verifique se a chave 'text' existe
             text = result['text']
-
             print(text)
             speak(text)
+
+            if text == 'que horas':
+                speak(core.SystemInfo.get_time())
